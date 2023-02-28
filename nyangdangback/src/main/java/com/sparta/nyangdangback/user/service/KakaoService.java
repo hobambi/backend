dfcb69facaf67eb334dd75cae9113763653ff6ee
aihtnyc_h <feiver@naver.com>
2023-03-01 00:40:58 +0900

    fix: nydangdangback # + "kakao로그인 수정"

--- nyangdangback/src/main/java/com/sparta/nyangdangback/user/service/KakaoService.java
+++ nyangdangback/src/main/java/com/sparta/nyangdangback/user/service/KakaoService.java
@@ -58,7 +58,7 @@ public class KakaoService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", "513dbab402347b4ce9abb443337b09a6");
-        body.add("redirect_uri", "http://localhost:8080/api/user/kakao/callback");
+        body.add("redirect_uri", "http://localhost:3000/api/user/kakao/callback");
         body.add("code", code);
 
         // HTTP 요청 보내기
